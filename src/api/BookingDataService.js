aa80edca47d0cb567d00c5d366a5a604070126c7
ArvidHolmqvist <arvidholmqvist98@gmail.com>
2021-04-25 21:47:22 +0200

    Update BookingDataService.js

--- src/api/BookingDataService.js
+++ src/api/BookingDataService.js
@@ -6,7 +6,7 @@ class BookingDataService {
      * @returns A JSON array of booking objects
      */
     retrieveAllBookings() {
-        return axios.get(`https://api-hamncafe-test.herokuapp.com/bookings`);
+        return axios.get(`http://api-hamncafe-test.herokuapp.com/bookings`);
     }
 
     /**
@@ -14,19 +14,19 @@ class BookingDataService {
      * @returns A JSON object of a Booking
      */
     retrieveBooking(id) {
-        return axios.get(`https://api-hamncafe-test.herokuapp.com/bookings/${id}`);
+        return axios.get(`http://api-hamncafe-test.herokuapp.com/bookings/${id}`);
     }
 
     updateBooking(id,booking) {
-        return axios.put(`https://api-hamncafe-test.herokuapp.com/bookings/${id}`, booking);
+        return axios.put(`http://api-hamncafe-test.herokuapp.com/bookings/${id}`, booking);
     }
 
     createBooking(booking) {
-        return axios.post(`https://api-hamncafe-test.herokuapp.com/bookings`, booking);
+        return axios.post(`http://api-hamncafe-test.herokuapp.com/bookings`, booking);
     }
 
     deleteBooking(id) {
-        return axios.delete(`https://api-hamncafe-test.herokuapp.com/bookings/${id}`);
+        return axios.delete(`http://api-hamncafe-test.herokuapp.com/bookings/${id}`);
     }
 
 
